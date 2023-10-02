9b7f500eeef060c87c181e90c482a682a4d8f2d5
AstroFoil <99050435+AstroFoil@users.noreply.github.com>
2023-10-02 11:17:38 +0200

    Docker and RabbitMQ
    Docker and RabbitMQ

---
+++ RabbitMQMicroservices/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace RabbitMQMicroservices;
+
+public class Project
+{
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+    
+}
