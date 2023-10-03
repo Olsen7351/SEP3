6878309384ea3ff8ce77371f80dd44a7495404db
EQKim <114055097+EQKim@users.noreply.github.com>
2023-10-03 10:59:18 +0200

    ProjectService

--- BlazorAppTEST/BlazorAppTEST/Program.cs
+++ BlazorAppTEST/BlazorAppTEST/Program.cs
@@ -8,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<ProjectService>();
+
+
 
 //Blazer Database
 builder.Services.AddScoped(s => new MongoDBService("mongodb+srv://Kim:1@sep3.uzgzawi.mongodb.net/SEP3?retryWrites=true&w=majority", "SEP3", "Projects"));
---
+++ BlazorAppTEST/BlazorAppTEST/Services/ProjectService.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using ProjectMicroservice.Models;
+
+namespace MainWeb.Services;
+
+public class ProjectService
+{
+    //HTTPClient
+    private readonly HttpClient httpClient;
+
+
+    public async Task CreateProjekt(Project projekt)
+    {
+        //Http client
+        using HttpClient client = new();
+        //Takes project and serialize to json
+        string createProjectToJson = JsonSerializer.Serialize(projekt);
+        //Magic
+        StringContent contentProject = new(createProjectToJson, Encoding.UTF8, "application/json");
+       
+        //Try and send it trough
+        HttpResponseMessage response = await client.PostAsync("https://localhost:5172/", contentProject);
+        string responseContent = await response.Content.ReadAsStringAsync();
+       
+        
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception($"Error:{response.StatusCode}, {contentProject}");
+        }
+    }
+
+
+    
+    
+    
+    
+    //Get
+    public async Task<ICollection<Project>> GetProjekt()
+    {
+        HttpResponseMessage response = await httpClient.GetAsync(("http://localhost:5172/"));
+        string contentProject = await response.Content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception($"Error:{response.StatusCode}, {contentProject}");
+        }
+
+        ICollection<Project> projects = JsonSerializer.Deserialize<ICollection<Project>>(contentProject, new JsonSerializerOptions());
+        return projects;
+    }
+}
