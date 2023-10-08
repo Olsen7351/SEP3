2e66190652e8d5c4f00d1c66bef65bd3acddec08
EQKim <114055097+EQKim@users.noreply.github.com>
2023-10-08 17:19:24 +0200

    Testing for service method "post"

--- SEP3_Solution/BlazorApp_Test/ServiceTesting.cs
+++ SEP3_Solution/BlazorApp_Test/ServiceTesting.cs
@@ -1,12 +1,54 @@
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using MainWeb.Services;
+using Moq;
+using Moq.Protected;
+using ProjectMicroservice.Models;
+using Xunit;
 
-namespace BlazorAppTest;
-
-public class ServiceTesting
+namespace BlazorAppTest
 {
-    [Fact]
-    public void Test1()
+    public class ProjectServiceTests
     {
-        //ProjectService projectService;
+        [Fact]
+        public async Task CreateProject_HTTP_POST()
+        {
+            // Arrange
+            var mockHandler = new Mock<HttpMessageHandler>();
+            mockHandler.Protected().Setup<Task<HttpResponseMessage>>
+                (
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent("{}"),
+                });
+
+            var client = new HttpClient(mockHandler.Object);
+            var service = new ProjectService(client);
+            var project = new Project
+            {
+                Id = 1,
+                Name = "Test Project",
+                Description = "This is a test project for unit testing.",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddDays(30)
+            };
+
+            // Act
+            await service.CreateProjekt(project);
+            
+            
+            // Assert
+            mockHandler.Protected().Verify("SendAsync", Times.Exactly(1),
+                ItExpr.IsAny<HttpRequestMessage>(),
+                ItExpr.IsAny<CancellationToken>()
+            );
+        }
     }
 }
