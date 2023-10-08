4b4a5d249b46e2a84e23e2f2a248613fff455380
EQKim <114055097+EQKim@users.noreply.github.com>
2023-10-08 16:28:45 +0200

    Revert "Attempting Name Change"
    This reverts commit 9fbf29c84e57868f05ec803b8a719b460b59ed0c.

--- SEP3_Solution/BlazorAppTest/UnitTest1.cs
+++ SEP3_Solution/BlazorAppTest/UnitTest1.cs
@@ -1,47 +1,15 @@
-using System.Net;
-using System.Net.Http;
-using System.Text;
-using System.Text.Json;
 using MainWeb.Services;
-using Moq;
-using Moq.Protected;
-using Xunit;
 
-namespace BlazorAppTest
+namespace BlazorAppTest;
+
+public class UnitTest1
 {
-    public class ProjectServiceTests
+    [Fact]
+    public void Test1()
     {
-        [Fact]
-        public async Task CreateProjekt_ShouldPostSuccessfully()
-        {
-            // Arrange
-            var mockHandler = new Mock<HttpMessageHandler>();
-            mockHandler.Protected()
-                .Setup<Task<HttpResponseMessage>>
-                (
-                    "SendAsync",
-                    ItExpr.IsAny<HttpRequestMessage>(),
-                    ItExpr.IsAny<CancellationToken>()
-                )
-                .ReturnsAsync(new HttpResponseMessage
-                {
-                    StatusCode = HttpStatusCode.OK,
-                    Content = new StringContent("{}"),
-                });
-
-            var client = new HttpClient(mockHandler.Object);
-            var service = new ProjectService(client);
-            var project = new Project
-            {
-                // fill in the properties here
-            };
-
-            // Act
-            await service.CreateProjekt(project);
-
-            // Assert
-            // If no exceptions were thrown, the test will pass.
-            // You can further assert that specific methods on the mock handler were called.
-        }
+        ProjectService projectService;
+        projectService.CreateProjekt();
+        
+        Assert
     }
 }
