71ec2eeb49fc59f95a9aa24b6e884b6a42673130
Fatema305502 <114063036+Fatema305502@users.noreply.github.com>
2023-10-16 11:37:04 +0200

    Mangler UI
    Mangler UI task(Kunne tilføje opgaver til backlog)

--- Broker/Program.cs
+++ Broker/Program.cs
@@ -4,8 +4,6 @@ using Broker.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Cors;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,25 +17,19 @@ builder.Services.AddSwaggerGen();
 
 //HTTP Client Projekter
 #if DEBUG //Development
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8004/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5131/") });
 #else //Production
-    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5002") });
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5002") });
 #endif
-
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(builder =>
-    {
-        builder
-            .AllowAnyOrigin() // Specify the allowed origins
-            .AllowAnyMethod() // Allow any HTTP method
-            .AllowAnyHeader(); // Allow any HTTP headers
-    });
-});
-
 
 builder.Services.AddScoped<IProjektService, ProjektService>();
 builder.Services.AddScoped<IBacklogService, BacklogService>();
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.HttpsPort = 443;
+    options.HttpsPort = 7103;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -48,7 +40,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseCors();
 
 app.UseAuthorization();
 
