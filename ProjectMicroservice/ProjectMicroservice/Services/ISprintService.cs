76202146fde2b0af32d33e6617674114ed661461
AstroFoil <99050435+AstroFoil@users.noreply.github.com>
2023-11-21 09:29:24 +0100

    Deleted files

---
+++ ProjectMicroservice/ProjectMicroservice/Services/ISprintService.cs
@@ -0,0 +1,7 @@
+namespace DefaultNamespace;
+
+public class IProjectSprint
+{
+    SprintBacklog createSprintBacklog(SprintBacklogRequest sprintBacklog);
+    SprintBacklog getSprintBacklog(SprintBacklog sprintBacklog);
+}
