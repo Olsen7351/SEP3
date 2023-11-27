2e022533c99a184cd3b31b3d7f4806514c8d2829
Fatema305502 <114063036+Fatema305502@users.noreply.github.com>
2023-11-27 10:08:40 +0100

    UserLogin_Test

--- BlazorAppTEST/BlazorAppTEST/Services/UserService.cs
+++ BlazorAppTEST/BlazorAppTEST/Services/UserService.cs
@@ -7,6 +7,12 @@ public class UserService : IUserService, IUserLogin
 {
     //HTTPClient
     private readonly HttpClient httpClient;
+    
+    // Constructor to inject the HTTPClient
+    public UserService(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
 
     public async Task createUser(User user)
     {
@@ -14,4 +20,18 @@ public class UserService : IUserService, IUserLogin
     }
     
     //login
+
+    public async Task<User> Login(User user)
+    {
+        HttpResponseMessage responseMessage = await httpClient.PostAsJsonAsync("api/Login", user);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return await responseMessage.Content.ReadFromJsonAsync<User>();
+        }
+        else
+        {
+            return null;
+        }
+      
+    }
 }
---
+++ SEP3_Solution/BlazorApp_Test/UserLogin_Test.cs
@@ -0,0 +1,27 @@
+using BlazorAppTEST.Services;
+using ClassLibrary_SEP3;
+using Xunit;
+using Task = System.Threading.Tasks.Task;
+
+namespace BlazorAppTest;
+
+public class UserLogin_Test
+{
+    [Fact]
+    public async Task LoginWithRightInformation()
+    {
+        var httpClient = new HttpClient();
+        var userService = new UserService(httpClient);
+
+        var user = new User
+        {
+            Username = "TestUser",
+            Password = "TestPassword"
+        };
+        
+        var loggedInUser = await userService.Login(user);
+        
+        Assert.NotNull(loggedInUser);
+        Assert.Equal("TestUser", loggedInUser.Username);
+    }
+}
