54f4834e7cc06685d475948afd1fa2b616b4b723
Fatema305502 <114063036+Fatema305502@users.noreply.github.com>
2023-11-28 12:13:58 +0100

    Delete ItaskService from Service directory and add to interface directory

--- BlazorAppTEST/BlazorAppTEST/Services/ITaskService.cs
+++
@@ -1,9 +0,0 @@
-using ProjectMicroservice.DataTransferObjects;
-
-namespace BlazorAppTEST.Services;
-
-public interface ITaskService
-{
-    Task<ClassLibrary_SEP3.Task?> CreateTask(AddBacklogTaskRequest task);
-    Task DeleteTask(string id, string projectId);
-}
