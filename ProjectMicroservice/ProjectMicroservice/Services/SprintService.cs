b88011cb2074313ea51a3c9c647d2fabd464000c
AstroFoil <99050435+AstroFoil@users.noreply.github.com>
2023-12-01 09:25:22 +0100

    Update SprintService.cs
    ProjectMicroservice

--- ProjectMicroservice/ProjectMicroservice/Services/SprintService.cs
+++ ProjectMicroservice/ProjectMicroservice/Services/SprintService.cs
@@ -1,14 +1,44 @@
-
-
+using System.Collections.Concurrent;
+using System.Data.Common;
 using ClassLibrary_SEP3;
+<<<<<<< Updated upstream
 
 namespace ProjectMicroservice.Services;
+=======
+>>>>>>> Stashed changes
 using ClassLibrary_SEP3.DataTransferObjects;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic.CompilerServices;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using ProjectMicroservice.Data;
+using ProjectMicroservice.Services;
+using ZstdSharp;
+using Task = ClassLibrary_SEP3.Task;
+namespace ProjectMicroservice.Services;
 
 public class SprintService : ISprintService
 {
 
+    private readonly IMongoCollection<Project> _project;
 
+    public SprintService(MongoDbContext context)
+    {
+        _project = context.Database.GetCollection<Project>("Projects");
+    }
+    
+    public SprintBacklog CreateSprintBacklog(CreateSprintBackLogRequest request)
+    {
+        var newSprintBackLog = new SprintBacklog()
+        {
+            ProjectId = request.projectId,
+            SprintBacklogId = request.Id,
+            Title = request.Title,
+            CreatedAt = DateTime.Today,
+            Tasks = new List<Task>()
+        };
+        _project.InsertOne();
+    }
     public List<SprintBacklog> GetAllSprintBacklogs(string projectId)
     {
         throw new NotImplementedException();
@@ -20,10 +50,7 @@ public class SprintService : ISprintService
         throw new NotImplementedException();
     }
 
-    public SprintBacklog CreateSprintBacklog(CreateSprintBackLogRequest request)
-    {
-        throw new NotImplementedException();
-    }
+
 
     public SprintBacklog UpdateSprintBacklog(string id, SprintBacklog sprintBacklog)
     {
@@ -32,6 +59,17 @@ public class SprintService : ISprintService
 
     public bool DeleteSprintBacklog(string id)
 
+    {
+        throw new NotImplementedException();
+    }
+
+    public SprintBacklog AddTaskToSprintBacklog(Task task, string id, string sprintBacklogId)
+    {
+        throw new NotImplementedException();
+
+    }
+
+    public List<Task> GetAllTasksForSprintBacklog(string id, string sprintBacklogId)
     {
         throw new NotImplementedException();
     }
