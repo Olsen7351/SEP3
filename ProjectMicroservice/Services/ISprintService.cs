febc0fb8593a967d79362c8be714427288dfe955
Olsen7351 <olsen7351@gmail.com>
2023-12-04 11:53:06 +0100

    New test project for microservice created

--- ProjectMicroservice/Services/ISprintService.cs
+++ ProjectMicroservice/Services/ISprintService.cs
@@ -3,7 +3,6 @@ using ClassLibrary_SEP3;
 using ClassLibrary_SEP3.DataTransferObjects;
 using Microsoft.VisualBasic.CompilerServices;
 using MongoDB.Bson;
-using ProjectMicroservice.Tests;
 using ProjectDatabase = ClassLibrary_SEP3.Project;
 using Task = ClassLibrary_SEP3.Task;
 
