007765166d02221d297e0d61c223ae1a08fb78ce
AstroFoil <99050435+AstroFoil@users.noreply.github.com>
2023-12-04 10:22:57 +0100

    Tests / Implementation
    Tests and implementation blazor and broker, addtaskstosprintbacklog

---
+++ BlazorAppTEST/BlazorAppTEST/Pages/SeeSprintBacklogPage.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorAppTEST.Pages;
+
+public class SeeSprintBacklogPage : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
