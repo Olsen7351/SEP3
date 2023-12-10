993ce34a01b09e9fa0398a741bd036c070f27d49
EQKim <114055097+EQKim@users.noreply.github.com>
2023-12-10 16:28:27 +0100

    Small fix

--- ClassLibrary_SEP3/DataTransferObjects/AddBacklogEntryRequest.cs
+++ ClassLibrary_SEP3/DataTransferObjects/AddBacklogEntryRequest.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Bson;
+﻿using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace ClassLibrary_SEP3.DataTransferObjects;
@@ -6,6 +7,8 @@ namespace ClassLibrary_SEP3.DataTransferObjects;
 public class AddBacklogEntryRequest
 {
     public String ProjectID { get; set; }
+    
+    [Required(ErrorMessage = "RequirmentNr is required")]
     public String RequirmentNr { get; set; }
     public String EstimateTime { get; set; }
     public String ActualTime { get; set; }
