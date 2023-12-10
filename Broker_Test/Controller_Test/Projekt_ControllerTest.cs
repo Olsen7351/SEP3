d780d34526f103c76ec4494f6c780cfbbdf12db0
Fatema305502 <114063036+Fatema305502@users.noreply.github.com>
2023-12-10 21:19:06 +0100

    Update projectController test with JWT token

--- Broker_Test/Controller_Test/Projekt_ControllerTest.cs
+++ Broker_Test/Controller_Test/Projekt_ControllerTest.cs
@@ -1,4 +1,6 @@
 using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
 using Broker.Controllers;
 using Broker.Services;
 using ClassLibrary_SEP3;
@@ -12,9 +14,111 @@ namespace Broker_Test.Controller_Test
 {
     public class ProjektControllerTests
     {
-        
+
         [Fact]
         public async void GetProjekt_ReturnsOk_WhenIdIsValid()
+        {
+            // Arrange
+            var mockProjektService = new Mock<IProjectService>();
+            string validId = "1"; // Valid Id
+
+            var project = new Project
+            {
+                Name = "Sample Project",
+                Description = "A description of the Sample Project",
+                StartDate = DateTime.UtcNow,
+                EndDate = DateTime.UtcNow.AddDays(30),
+
+            };
+            mockProjektService.Setup(service => service.GetProjekt(validId))
+                .ReturnsAsync(project);
+
+            var mockHttpContext = new Mock<HttpContext>();
+            var mockHttpRequest = new Mock<HttpRequest>();
+            var mockHeaders = new HeaderDictionary();
+
+            // Mock JWT Token with "sub" claim set to "Alma" for rabbitMQ
+            var username = "Alma";
+            var payload = new Dictionary<string, object> { { "sub", username } };
+            var payloadJson = JsonSerializer.Serialize(payload);
+            var payloadBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(payloadJson));
+            var mockJwtToken = $"header.{payloadBase64}.signature";
+
+            mockHeaders["Authorization"] = "Bearer " + mockJwtToken;
+            mockHttpRequest.Setup(r => r.Headers).Returns(mockHeaders);
+            mockHttpContext.SetupGet(ctx => ctx.Request).Returns(mockHttpRequest.Object);
+
+            var controller = new BrokerProjectController(mockProjektService.Object);
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = mockHttpContext.Object
+            };
+
+            // Act
+            var result = await controller.GetProjekt(validId);
+
+            var okResult = Assert.IsType<Project>(result);
+            Assert.IsType<Project>(okResult);
+            Assert.Equal(project, okResult);
+
+            mockProjektService.Verify(service => service.GetProjekt(validId), Times.Once);
+        }
+
+        [Fact]
+        public async void CreateProjekt_ReturnsOk_WhenProjectIsNotNull()
+        {
+            // Arrange
+            var mockProjektService = new Mock<IProjectService>();
+            var controller = new BrokerProjectController(mockProjektService.Object);
+
+            var verifiedUsername = "Alma";
+            var projectRequest = new CreateProjectRequest
+            {
+                Name = "Sample Project",
+                Description = "A description of the Sample Project",
+                StartDate = DateTime.UtcNow,
+                EndDate = DateTime.UtcNow.AddDays(30),
+                ByUsername = verifiedUsername
+            };
+
+            mockProjektService.Setup(service => service.CreateProjekt(It.IsAny<CreateProjectRequest>()))
+                .ReturnsAsync(new OkResult());
+
+            var claims = new List<Claim> { new Claim(ClaimTypes.NameIdentifier, verifiedUsername) };
+            var identity = new ClaimsIdentity(claims, "TestAuthType");
+            var claimsPrincipal = new ClaimsPrincipal(identity);
+
+            var mockHttpContext = new Mock<HttpContext>();
+            var mockHttpRequest = new Mock<HttpRequest>();
+            var mockHeaders = new HeaderDictionary();
+
+            // Mock JWT Token with "sub" claim set to "Alma"
+            var username = "Alma";
+            var payload = new Dictionary<string, object> { { "sub", username } };
+            var payloadJson = JsonSerializer.Serialize(payload);
+            var payloadBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(payloadJson));
+            var mockJwtToken = $"header.{payloadBase64}.signature";
+
+            mockHeaders["Authorization"] = "Bearer " + mockJwtToken;
+            mockHttpRequest.Setup(r => r.Headers).Returns(mockHeaders);
+            mockHttpContext.SetupGet(ctx => ctx.Request).Returns(mockHttpRequest.Object);
+            mockHttpContext.SetupGet(ctx => ctx.User).Returns(claimsPrincipal); // Adding the User property
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = mockHttpContext.Object
+            };
+
+            // Act
+            var result = await controller.CreateProjekt(projectRequest);
+
+            // Assert
+            var okResult = Assert.IsType<OkResult>(result);
+
+        }
+
+        [Fact]
+        public async void GetProjekt_ReturnsProject_WhenIdIsValid()
         {
             // Arrange
             var mockProjektService = new Mock<IProjectService>();
@@ -24,79 +128,79 @@ namespace Broker_Test.Controller_Test
             var project = new Project
             {
                 Id = "1",
-                Name = "Alma",
-                Description = "Brush Alma",
+                Name = "Sample Project",
+                Description = "A description of the Sample Project",
             };
             mockProjektService.Setup(service => service.GetProjekt(validId))
                 .ReturnsAsync(project);
 
+            var mockHttpContext = new Mock<HttpContext>();
+            var mockHttpRequest = new Mock<HttpRequest>();
+            var mockHeaders = new HeaderDictionary();
+
+            // Mock JWT Token with "sub" claim set to "Alma"
+            var username = "Alma";
+            var payload = new Dictionary<string, object> { { "sub", username } };
+            var payloadJson = JsonSerializer.Serialize(payload);
+            var payloadBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(payloadJson));
+            var mockJwtToken = $"header.{payloadBase64}.signature";
+
+            mockHeaders["Authorization"] = "Bearer " + mockJwtToken;
+            mockHttpRequest.Setup(r => r.Headers).Returns(mockHeaders);
+            mockHttpContext.SetupGet(ctx => ctx.Request).Returns(mockHttpRequest.Object);
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = mockHttpContext.Object
+            };
+
             // Act
             var result = await controller.GetProjekt(validId);
 
-            var okResult = Assert.IsType<Project>(result);
-            Assert.IsType<Project>(okResult);
-            Assert.Equal(project, okResult);
-        }
-        
-        [Fact]
-        public async void CreateProjekt_ReturnsOk_WhenProjectIsNotNull()
-        {
-            var mockProjektService = new Mock<IProjectService>();
-            var controller = new BrokerProjectController(mockProjektService.Object);
-
-            var verifiedUsername = "Alma";
-            var projectRequest = new CreateProjectRequest
-            {
-                Name = "Sample Project", 
-                Description = "A description of the Sample Project", 
-                StartDate = DateTime.UtcNow,
-                EndDate = DateTime.UtcNow.AddDays(30),
-                ByUsername = verifiedUsername 
-            };
-            
-            mockProjektService.Setup(service => service.CreateProjekt(It.IsAny<CreateProjectRequest>()))
-                .ReturnsAsync(new OkResult());
-            
-            var claims = new List<Claim> { new Claim(ClaimTypes.NameIdentifier, verifiedUsername) };
-            var identity = new ClaimsIdentity(claims, "TestAuthType");
-            var claimsPrincipal = new ClaimsPrincipal(identity);
-
-            var mockHttpContext = new DefaultHttpContext
-            {
-                User = claimsPrincipal
-            };
-            controller.ControllerContext = new ControllerContext()
-            {
-                HttpContext = mockHttpContext
-            };
-
-            // Act
-            var result = await controller.CreateProjekt(projectRequest);
-
             // Assert
-            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull(result);
+            var projectResult = Assert.IsType<Project>(result);
+            Assert.Equal(project, projectResult);
+
+            // Verifying the service call
+            mockProjektService.Verify(service => service.GetProjekt(validId), Times.Once);
         }
-        
-        [Fact]
+    
+
+    [Fact]
         public async void CreateProjekt_ReturnsBadRequest_WhenProjectIsNull()
         {
-            // Arrange
-            var mockProjektService = new Mock<IProjectService>();
-            var controller = new BrokerProjectController(mockProjektService.Object);
+                // Arrange
+                var mockProjektService = new Mock<IProjectService>();
+                var controller = new BrokerProjectController(mockProjektService.Object);
 
-            // Mock HttpContext for Authorization header
-            var mockHttpContext = new DefaultHttpContext();
-            mockHttpContext.Request.Headers["Authorization"] = "Bearer testtoken";
-            controller.ControllerContext = new ControllerContext()
-            {
-                HttpContext = mockHttpContext
-            };
+                // Mock HttpContext for Authorization header
+                var mockHttpContext = new Mock<HttpContext>();
+                var mockHttpRequest = new Mock<HttpRequest>();
+                var mockHeaders = new HeaderDictionary();
 
-            // Act
-            var result = await controller.CreateProjekt(null); // Passing null directly
+                // Mock JWT Token with "sub" claim set to "Alma"
+                var username = "Alma";
+                var payload = new Dictionary<string, object> { { "sub", username } };
+                var payloadJson = JsonSerializer.Serialize(payload);
+                var payloadBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(payloadJson));
+                var mockJwtToken = $"header.{payloadBase64}.signature";
 
-            // Assert
-            Assert.IsType<BadRequestResult>(result);
+                mockHeaders["Authorization"] = "Bearer " + mockJwtToken;
+                mockHttpRequest.Setup(r => r.Headers).Returns(mockHeaders);
+                mockHttpContext.SetupGet(ctx => ctx.Request).Returns(mockHttpRequest.Object);
+
+                controller.ControllerContext = new ControllerContext
+                {
+                    HttpContext = mockHttpContext.Object
+                };
+
+                // Act
+                var result = await controller.CreateProjekt(null); // Passing null directly
+
+                // Assert
+                Assert.IsType<BadRequestResult>(result);
+            
         }
         [Fact]
         public async void AddUserToProject_ReturnsOk_WhenRequestIsValid()
@@ -116,13 +220,36 @@ namespace Broker_Test.Controller_Test
                 service => service.AddUserToProject(request)
             ).ReturnsAsync(new OkResult());
 
+            var mockHttpContext = new Mock<HttpContext>();
+            var mockHttpRequest = new Mock<HttpRequest>();
+            var mockHeaders = new HeaderDictionary();
+
+            // Mock JWT Token with "sub" claim set to "Alma"
+            var username = "Alma";
+            var payload = new Dictionary<string, object> { { "sub", username } };
+            var payloadJson = JsonSerializer.Serialize(payload);
+            var payloadBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(payloadJson));
+            var mockJwtToken = $"header.{payloadBase64}.signature";
+
+            mockHeaders["Authorization"] = "Bearer " + mockJwtToken;
+            mockHttpRequest.Setup(r => r.Headers).Returns(mockHeaders);
+            mockHttpContext.SetupGet(ctx => ctx.Request).Returns(mockHttpRequest.Object);
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = mockHttpContext.Object
+            };
+
             // Act
             var result = await controller.AddUserToProject(request);
 
             // Assert
             var okResult = Assert.IsType<OkObjectResult>(result);
-            Assert.IsType<OkObjectResult>(okResult); 
-           // Assert.Equal("Some content", okResult.Value.ToString()); // Corrected line
+            Assert.IsType<OkObjectResult>(okResult);
+            // Assert.Equal("Some content", okResult.Value.ToString()); // Uncomment and correct as needed
+
+            // Verifying the service call
+            mockProjektService.Verify(service => service.AddUserToProject(request), Times.Once);
         }
     }
 }
